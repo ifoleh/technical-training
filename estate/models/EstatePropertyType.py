e88fd70d1b6c8894ded5f5536978ec0543a5b3d3
hl <herbert.leberbauer@gmx.at>
2024-09-30 15:11:57 +0200

    Propertytype

---
+++ estate/models/EstatePropertyType.py
@@ -0,0 +1,8 @@
+from odoo import fields, models
+
+class EstatePropertyType(models.Model):
+    _name = "estate.propertytype"
+    _description = "Types of Properties (House, Flat, ...)"
+    
+    name = fields.Char(required = True, string='Name')
+    description = fields.Text(string='Beschreibung')
