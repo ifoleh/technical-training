f328c9f7eac4dd5a785daf4f60e8ec8cc5aae09d
hl <herbert.leberbauer@gmx.at>
2024-09-30 15:43:05 +0200

    ci

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,11 +1,12 @@
 {
     "name": "Estate",  # The name that will appear in the App list
-    "version": "17.0.0.21",  # Version
+    "version": "17.0.0.23",  # Version
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["base"],  # dependencies
     "data": [
         "security/ir.model.access.csv",
-        "views/estate_property_views.xml"
+        "views/estate_property_views.xml",
+        "views/estate_propertytype_views.xml"
     ],
     "installable": True,
     'license': 'LGPL-3',
