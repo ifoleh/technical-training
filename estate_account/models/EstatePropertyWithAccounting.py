f1f38535a9db78fc30a6f6eb20987863ba36622c
hl <herbert.leberbauer@gmx.at>
2024-10-01 15:44:43 +0200

    c77

--- estate_account/models/EstatePropertyWithAccounting.py
+++ estate_account/models/EstatePropertyWithAccounting.py
@@ -1,7 +1,9 @@
-from odoo import models
+from odoo import models, fields
 
 class EstatePropertyWithAccounting(models.Model):
     _inherit = "estate.property"
+
+    onlyVisibleWithAccounting = fields.Boolean(string="Only visible with accounting", default=False)
 
     def action_create_invoice(self):
         pass
