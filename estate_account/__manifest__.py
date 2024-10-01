68bdf82d68906d992a6033d21f627406eec935ba
hl <herbert.leberbauer@gmx.at>
2024-10-01 15:36:00 +0200

    c75

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Estate Accounting",  # The name that will appear in the App list
+    "version": "17.0.10.1.1",  # Version
+    "application": True,  # This line says the module is an App, and not a module
+    "depends": ["base","estate"],  # dependencies
+    "data": [
+    ],
+    "installable": True,
+    'license': 'LGPL-3',
+}
