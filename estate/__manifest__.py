e10e0ca9a300e412b4c062e44a5e172d8c78943f
hl <herbert.leberbauer@gmx.at>
2024-10-01 10:51:14 +0200

    c37

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Estate",  # The name that will appear in the App list
-    "version": "17.0.10.1.21",  # Version
+    "version": "17.0.10.1.22",  # Version
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["base"],  # dependencies
     "data": [
