c1772b6e9a3c25df39888cd6cf536a8c12390adb
hl <herbert.leberbauer@gmx.at>
2024-10-01 15:06:23 +0200

    c73

--- estate/models/EstatePropertyType.py
+++ estate/models/EstatePropertyType.py
@@ -11,3 +11,18 @@ class EstatePropertyType(models.Model):
     def _compute_properties_count(self):
         for property_type in self:
             property_type.properties_count = len(property_type.property_ids)       
+
+    def action_show_offers(self):
+        return {
+            "type": "ir.actions.act_window",
+            "name": "Estate Properties Show Action",
+            "res_model": "estate.property",
+            "view_mode": "list,form"
+        }
+
+    # <record id="action_show_offers" model="ir.actions.act_window">
+    #     <field name="name">Estate Properties Show Action</field>
+    #     <field name="res_model">estate.property</field>
+    #     <field name="view_mode">list,form</field>
+    # </record>
+
